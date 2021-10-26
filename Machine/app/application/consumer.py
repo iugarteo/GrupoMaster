680385ef50ad065f7b918b623f9884757b73590f
josualbizu <90928013+josualbizu@users.noreply.github.com>
2021-10-26 22:00:41 +0200

    Publish Order.md --> Delivery y Machine Escuchan

--- Machine/app/application/consumer.py
+++ Machine/app/application/consumer.py
@@ -1,92 +1,31 @@
-import json
-from types import SimpleNamespace
-
 import pika
-from . import Config
+from .checkJWT import setKey
 
-
-from Machine.app.application.publisher import publish_event
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy import create_engine
-
-from .checkJWT import set_public_key
-from .machine import Machine
-from .models import PieceGroup, Piece
-
-engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
-Session = scoped_session(
-            sessionmaker(
-                autocommit=False,
-                autoflush=True,
-                bind=engine)
-        )
-
-public_key = None
-
-
-def init_rabbitmq_key():
+def init_rabbitmq():
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='192.168.17.2'))
+        #pika.ConnectionParameters(host='192.168.17.2'))
+        pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     channel.exchange_declare(exchange='global', exchange_type='topic', durable=True)
+    #channel.exchange_declare(exchange='services', exchange_type='topic', durable=True)
 
-    result = channel.queue_declare('machine_key', durable=True)
+    result = channel.queue_declare('order_create_queue', durable=True)
+    #result = channel.queue_declare('services', durable=True)
     queue_name = result.method.queue
 
     channel.queue_bind(
-        exchange='global', queue="machine_key", routing_key="client.key")
+        exchange='global', queue="order_create_queue", routing_key="order.md")
+    #channel.queue_bind(
+        #exchange='services', queue="services", routing_key="*.*")
 
     channel.basic_consume(
-        queue=queue_name, on_message_callback=callback_key, auto_ack=True)
-
-    print("Waiting for key...")
-    channel.start_consuming()
-
-
-def init_rabbitmq_event():
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='192.168.17.2'))
-    channel = connection.channel()
-    channel.exchange_declare(exchange='global', exchange_type='topic', durable=True)
-
-    result = channel.queue_declare('machine', durable=True)
-    queue_name = result.method.queue
-
-    channel.queue_bind(
-        exchange='global', queue="machine", routing_key="order.md")
-
-    channel.basic_consume(
-        queue=queue_name, on_message_callback=callback_event, auto_ack=True)
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
 
     print("Waiting for event...")
     channel.start_consuming()
 
 
-def callback_key(ch, method, properties, body):
+def callback(ch, method, properties, body):
     print(" [x] {} {}".format(method.routing_key, body))
-    set_public_key(body)
-
-
-def callback_event(ch, method, properties, body):
-    print(" [x] {} {}".format(method.routing_key, body))
-    message = json.loads(body, object_hook=lambda d: SimpleNamespace(**d))
-    my_machine = Machine()
-    session = Session()
-    try:
-        new_PieceGroup = PieceGroup(
-            order_id=message['order_id'],
-            number_of_pieces=message['number_of_pieces'],
-            status="Created"
-        )
-        session.add(new_PieceGroup)
-        for i in range(new_PieceGroup.number_of_pieces):
-            piece = Piece()
-            piece.group = new_PieceGroup
-            session.add(piece)
-        session.commit()
-        my_machine.add_pieces_to_queue(new_PieceGroup.pieces)
-        session.commit()
-    except KeyError:
-        session.rollback()
-        session.close()
+    setKey(body)
 
