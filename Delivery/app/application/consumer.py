b39084a3cc5997483564ff78a515677028aed9b7
josualbizu <90928013+josualbizu@users.noreply.github.com>
2021-10-26 21:59:18 +0200

    Publish Order.md --> Delivery y Machine Escuchan

--- Delivery/app/application/consumer.py
+++ Delivery/app/application/consumer.py
@@ -5,17 +5,21 @@ public_key = None
 
 def init_rabbitmq():
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='192.168.17.2'))
+        #pika.ConnectionParameters(host='192.168.17.2'))
+        pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     channel.exchange_declare(exchange='global', exchange_type='topic', durable=True)
     #channel.exchange_declare(exchange='services', exchange_type='topic', durable=True)
 
     result = channel.queue_declare('delivery', durable=True)
+    result = channel.queue_declare('order_create_queue', durable=True)
     #result = channel.queue_declare('services', durable=True)
     queue_name = result.method.queue
 
     channel.queue_bind(
         exchange='global', queue="delivery", routing_key="client.key")
+    channel.queue_bind(
+        exchange='global', queue="order_create_queue", routing_key="order.md")
     #channel.queue_bind(
         #exchange='services', queue="services", routing_key="*.*")
 
