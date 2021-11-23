94102f93d097ed6cdb1b72c359e2ce0ee09646d3
Iñigo Navarro <33113628+InigoNavarro@users.noreply.github.com>
2021-11-23 11:25:32 +0100

    Todo lo demas - delivery no pilla ocultos

---
+++ private/TodoLoDemas/delivery/app/application/consumer.py
@@ -0,0 +1,88 @@
+import json
+
+import pika
+from . import Config
+
+from .checkJWT import write_public_key_to_file
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
+
+from .delivery import update_delivery_by_order, create_delivery
+from .models import Delivery
+
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+Session = scoped_session(
+            sessionmaker(
+                autocommit=False,
+                autoflush=True,
+                bind=engine)
+        )
+
+public_key = None
+
+
+def init_rabbitmq_key():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=Config.RABBIT_IP))
+       
+    channel = connection.channel()
+    channel.exchange_declare(exchange='global', exchange_type='topic', durable=True)
+
+    result = channel.queue_declare('delivery_key', durable=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(
+        exchange='global', queue="delivery_key", routing_key="client.key")
+
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback_key, auto_ack=True)
+
+    print("Waiting for key...")
+    channel.start_consuming()
+
+
+def init_rabbitmq_event(queue, routing_key, callback):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=Config.RABBIT_IP))
+    channel = connection.channel()
+    channel.exchange_declare(exchange='global', exchange_type='topic', durable=True)
+
+    result = channel.queue_declare(queue, durable=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(
+        exchange='global', queue=queue, routing_key=routing_key)
+
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+    print("Waiting for event...")
+    channel.start_consuming()
+
+
+def callback_key(ch, method, properties, body):
+    print(" [x] {} {}".format(method.routing_key, body))
+    write_public_key_to_file(body)
+
+
+def callback_order_event(ch, method, properties, body):
+    print(" [x] {} {}".format(method.routing_key, body))
+    message = json.loads(body)
+    session = Session()
+    delivery = create_delivery(session, message["order_id"])
+    if not delivery:
+        print("Error during delivery registration with {} order_id".format(message.order_id))
+    session.close()
+
+
+def callback_machine_event(ch, method, properties, body):
+    print(" [x] {} {}".format(method.routing_key, body))
+    message = json.loads(body)
+    session = Session()
+    delivery = update_delivery_by_order(session, message["order_id"], Delivery.STATUS_SENT)
+    if delivery:
+        # sleep for 10 seconds
+        update_delivery_by_order(session, message["order_id"], Delivery.STATUS_RECEIVED)
+    else:
+        print("Error while updating delivery with {} order_id".format(message.order_id))
+    session.close()
