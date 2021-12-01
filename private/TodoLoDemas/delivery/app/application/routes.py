2c1080904401087172e9d00b691c8b7567698493
Iñigo Navarro <33113628+InigoNavarro@users.noreply.github.com>
2021-12-01 13:47:32 +0100

    Posibles outputs Health - D

--- private/TodoLoDemas/delivery/app/application/routes.py
+++ private/TodoLoDemas/delivery/app/application/routes.py
@@ -77,7 +77,7 @@ def update_status_received(id):
 def health_check():
  #abort(BadRequest)
 #if(machine.state == "Free"):
-    #messagge"The service Order is up and free, give it some work."
+    #messagge = "The service Order is up and free, give it some work."
  #if(machine.state == "Working"):
     #messagge = "The service Order is up but currently working, wait a little." 
 #if(machine.state == "Down"):
