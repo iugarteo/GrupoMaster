271c6f7200e6089d2376569a31fea78f5ee7f32a
Iñigo Navarro <33113628+InigoNavarro@users.noreply.github.com>
2021-12-03 09:09:35 +0100

    Health extra M, comentado, no testeado - psu

--- private/TodoLoDemas/machine/app/application/routes.py
+++ private/TodoLoDemas/machine/app/application/routes.py
@@ -5,7 +5,9 @@ from werkzeug.exceptions import NotFound, InternalServerError, BadRequest, Unsup
 import traceback
 from .machine import Machine
 from . import Session, checkJWT
-
+import psutil
+import os
+from sqlalchemy import create_engine
 my_machine = Machine()
 
 # Order Routes #########################################################################################################
@@ -100,15 +102,40 @@ def view_machine_status():
 
 @app.route('/machine/health', methods=['HEAD', 'GET'])
 def health_check():
- #abort(BadRequest)
-#if(machine.state == "Free"):
-    #messagge = "The service Order is up and free, give it some work."
- #if(machine.state == "Working"):
-    #messagge = "The service Order is up but currently working, wait a little." 
-#if(machine.state == "Down"):
-    #messagge = "The machine is down, we are working on it."
-#return messagge
- return "OK"
+#abort(BadRequest)
+	#if(machine.state == "Free"):
+	    #messagge = "The service Order is up and free, give it some work."
+	 #if(machine.state == "Working"):
+	    #messagge = "The service Order is up but currently working, wait a little." 
+	#if(machine.state == "Down"):
+	    #messagge = "The machine is down, we are working on it."
+	###Extra
+	#fichero = os.path.exists("./public_key.pem")
+	#if (fichero == True):
+	#	testFich = "Existe una pubkey en este servicio"
+	#else:
+	#	testFich =  "No existe pubkey en este servicio"
+
+	#cpuTest = psutil.cpu_percent(1)
+	#ramTest = psutil.virtual_memory().percent
+	#memTest = (psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
+
+	#engineTest = create_engine(Config.SQLALCHEMY_DATABASE_URI) ##Supongo que este engine luego habria que cerrarlo, 
+								## Lo unico encontrado es dispose(), pero no se si es eso
+	#if(engineTest.connect()):
+	#	conexionDB = "Es posible la conexión con la BD"
+	#if(!engineTest.connect()):
+	#	conexionDB = "No es posible la conexión con la BD"
+
+	#response = jsonify(estado = messagge,
+	#pubKey=testFich,
+	#cpu=cpuTest,
+	#ram= ramTest, 
+	#mem = memTest,
+	#db = conexionDB)
+
+	#return response
+	return "Ok"
 
 # Error Handling #######################################################################################################
 @app.errorhandler(UnsupportedMediaType)
