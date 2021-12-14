6b40dc740240aa090468c444e7b0693b8cc0231a
Iñigo Navarro <33113628+InigoNavarro@users.noreply.github.com>
2021-12-14 17:49:19 +0100

    Health P

--- private/TodoLoDemas/payment/app/application/routes.py
+++ private/TodoLoDemas/payment/app/application/routes.py
@@ -149,40 +149,14 @@ def add_money():
 # Health Check ################
 @app.route('/payment/health', methods=['HEAD', 'GET'])
 def health_check():
- #abort(BadRequest)
-	#if(machine.state == "Free"):
-	    #messagge = "The service Order is up and free, give it some work."
-	 #if(machine.state == "Working"):
-	    #messagge = "The service Order is up but currently working, wait a little." 
-	#if(machine.state == "Down"):
-	    #messagge = "The machine is down, we are working on it."
-	###Extra
-	#fichero = os.path.exists("./public_key.pem")
-	#if (fichero == True):
-	#	testFich = "Existe una pubkey en este servicio"
-	#else:
-	#	testFich =  "No existe pubkey en este servicio"
-
-	#cpuTest = psutil.cpu_percent(1)
-	#ramTest = psutil.virtual_memory().percent
-	#memTest = (psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
-
-	#engineTest = create_engine(Config.SQLALCHEMY_DATABASE_URI) ##Supongo que este engine luego habria que cerrarlo, 
-								## Lo unico encontrado es dispose(), pero no se si es eso
-	#if(engineTest.connect()):
-	#	conexionDB = "Es posible la conexión con la BD"
-	#if(!engineTest.connect()):
-	#	conexionDB = "No es posible la conexión con la BD"
-
-	#response = jsonify(estado = messagge,
-	#pubKey=testFich,
-	#cpu=cpuTest,
-	#ram= ramTest, 
-	#mem = memTest,
-	#db = conexionDB)
-
-	#return response
-	return "Ok"
+	fichero = os.path.exists("./public_key.pem")
+	bool ok = False
+	cpuTest = psutil.cpu_percent(1)
+	ramTest = psutil.virtual_memory().percent
+	if (fichero == True and cpuTest <= 50 and ramTest <= 50):
+		return "Ok"
+	else:
+		return "No Ok"
 
 # Error Handling #######################################################################################################
 @app.errorhandler(UnsupportedMediaType)
