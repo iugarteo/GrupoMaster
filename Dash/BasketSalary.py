a28f1c76e47362ad573b4b1ced5574cbe9118535
Eñaut Mendiluze <mendienaut@gmail.com>
2021-12-29 21:10:01 +0100

    Grid bonito

--- Dash/BasketSalary.py
+++ Dash/BasketSalary.py
@@ -2,6 +2,7 @@ import dash
 from dash import dcc, State
 from dash import html
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 import plotly.express as px
 import pandas as pd
@@ -9,7 +10,6 @@ import warnings
 from prediction import get_model, get_prediction, create_table
 
 warnings.filterwarnings("ignore")
-
 
 df = pd.read_csv('2017-18_NBA_salary.csv')
 df2 = pd.read_csv('players_stats.csv')
@@ -22,161 +22,195 @@ minutosL = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
 edadL = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41]
 shotsL = [0, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90,
           0.95, 1]
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.SKETCHY]
+
+
 modelo1, scores1 = get_model("RF")
 modelo2, scores2 = get_model("DF")
 modelo3, scores3 = get_model("LR")
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-
-    html.H3('Algorithm Type'),
-    dcc.Dropdown(
-        id='AlgType',
-        options=[
-            {'label': 'Random Forest', 'value': 'RF'},
-            {'label': 'Decision Forest', 'value': 'DF'},
-            {'label': 'Linear Regresion', 'value': 'LR'}
-        ],
-        value='RF'
+    dbc.Row(
+        dbc.Col(
+            html.H1('Predice tu salario'),
+        )
     ),
-    html.Div([
-
-        # R^2
-        html.Div([
-            html.H3("R^2"),
-            html.H6(id="R2_text")
-        ],
-            id="R2-score",
-            className="mini_container indicator",
-        ),
-        # MAE
-        html.Div([
-            html.H3("MAE"),
-            html.H6(id="MAE_text")
-        ],
-            id="MAE-score",
-            className="mini_container indicator",
-        ),
-        # RMSE
-        html.Div([
-            html.H3("RMSE"),
-            html.H6(id="RMSE_text")
-        ],
-            id="RMSE-score",
-            className="mini_container indicator",
-        ),
+    html.Br(),
+    html.Div([dbc.Card(dbc.CardBody(
+        dbc.Row(
+            [
+                dbc.Col(html.Div([html.H3('Seleccionar algoritmo'),
+                                  dcc.Dropdown(
+                                      id='AlgType',
+                                      options=[
+                                          {'label': 'Random Forest', 'value': 'RF'},
+                                          {'label': 'Decision Tree', 'value': 'DF'},
+                                          {'label': 'Linear Regresion', 'value': 'LR'}
+                                      ],
+                                      value='RF',
+                                      style={'font-size':'16px'}
+                                  ),])),
+                dbc.Col(# R^2
+                    html.Div([
+                        html.H3("R^2"),
+                        html.H3(id="R2_text")
+                    ],
+                        id="R2-score",
+                        className="mini_container indicator",
+                    ),),
+                dbc.Col(# MAE
+                    html.Div([
+                        html.H3("MAE"),
+                        html.H3(id="MAE_text")
+                    ],
+                        id="MAE-score",
+                        className="mini_container indicator",
+                    ),),
+                dbc.Col(# RMSE
+                    html.Div([
+                        html.H3("RMSE"),
+                        html.H3(id="RMSE_text")
+                    ],
+                        id="RMSE-score",
+                        className="mini_container indicator",
+                    ),),
+            ]
+        ))),
     ],
         id="indicators",
     ),
+    html.Br(),
     html.Div([
-        html.H3("% Shots:"),
-        dcc.Slider(
-            id='shots',
-            min=0,
-            max=1,
-            value=0,
-            step=0.05,
-            marks={str(shot): str(shot) for shot in shotsL},
-            tooltip={"placement": "bottom", "always_visible": True},
+        dbc.Row(
+            [
+                dbc.Col(dbc.Card(dbc.CardBody(html.Div([html.H3("% Tiros acertado:"),
+                                                        dcc.Slider(
+                                                            id='shots',
+                                                            min=0,
+                                                            max=1,
+                                                            value=0,
+                                                            step=0.05,
+                                                            marks={str(shot): str(shot) for shot in shotsL},
+                                                            tooltip={"placement": "bottom", "always_visible": True}
+                                                        ),
+                                                        html.Br(),
+                                                        html.H3("Edad:"),
+                                                        dcc.Slider(
+                                                            id='Age',
+                                                            min=19,
+                                                            max=41,
+                                                            value=25,
+                                                            step=1,
+                                                            marks={str(edad): str(edad) for edad in edadL},
+                                                            tooltip={"placement": "bottom", "always_visible": True},
+                                                        ),
+                                                        html.Br(),
+                                                        html.H3("Minutos jugados:"),
+                                                        dcc.Slider(
+                                                            id='mins',
+                                                            min=0,
+                                                            max=45,
+                                                            value=0,
+                                                            step=1,
+                                                            marks={str(minutos): str(minutos) for minutos in minutosL},
+                                                            tooltip={"placement": "bottom", "always_visible": True},
+                                                        ),
+                                                        html.Br(),
+                                                        html.H3("Salario minimo requerido:"),
+                                                        html.Div([dcc.Input(id='input-on-submit', type='number'), html.Button('Mostrar minimo', id='submit-val', n_clicks=0)]),
+                                                        ])), style={'height':'70vh'}), width=5),
+                dbc.Col(dbc.Card(dbc.CardBody([html.H3("Salario a cobrar por equipo:"),html.Div(dcc.Graph(id='graphAlg'))]), style={'height':'70vh'}), width=7),
+            ]
         ),
-        html.H3("Age:"),
-        dcc.Slider(
-            id='Age',
-            min=19,
-            max=41,
-            value=25,
-            step=1,
-            marks={str(edad): str(edad) for edad in edadL},
-            tooltip={"placement": "bottom", "always_visible": True},
-        ),
-        html.H3("Minutos jugados:"),
-        dcc.Slider(
-            id='mins',
-            min=0,
-            max=45,
-            value=0,
-            step=1,
-            marks={str(minutos): str(minutos) for minutos in minutosL},
-            tooltip={"placement": "bottom", "always_visible": True},
-        ),
-        html.H3("Salario minimo querido:"),
-        html.Div(dcc.Input(id='input-on-submit', type='number')),
-        html.Button('Mostrar threshold', id='submit-val', n_clicks=0),
-        dcc.Graph(id='graphAlg'),
     ]),
+    html.Br(),
     html.Div([
-        html.H3('Graph Type'),
-        dcc.Dropdown(
-            id='graphType',
-            options=[
-                {'label': 'Tarta', 'value': 'pie'},
-                {'label': 'Barchart', 'value': 'bar'}
-            ],
-            value='pie'
+        dbc.Row(
+            dbc.Col(
+                html.H1('Estadisticas generales NBA'),
+                width={"size": 6, "offset": 3},
+            )
         ),
-        html.Div([
-            html.H3('Equipos'),
-            dcc.Dropdown(
-                options=[
-                    {'label': 'Los Angeles Lakers', 'value': 'LAL'},
-                    {'label': 'Toronto Raptors', 'value': 'TOR'},
-                    {'label': 'Utah Jazz', 'value': 'UTA'},
-                    {'label': 'Philadelphia 76ers', 'value': 'PHI'},
-                    {'label': 'Boston Celtics', 'value': 'BOS'},
-                    {'label': 'Golden State Warriors', 'value': 'GSW'},
-                    {'label': 'Los Angeles Clippers', 'value': 'LAC'},
-                    {'label': 'New Orleans Pelicans', 'value': 'NOP'},
-                    {'label': 'Charlotte Bobcats', 'value': 'CHA'},
-                    {'label': 'Phoenix Suns', 'value': 'PHX'},
-                    {'label': 'Washington Wizards', 'value': 'WAS'},
-                    {'label': 'Memphis Grizzlies', 'value': 'MEM'},
-                    {'label': 'New York Knicks', 'value': 'NYK'},
-                    {'label': 'Dallas Mavericks', 'value': 'DAL'},
-                    {'label': 'Milwakee Bucks', 'value': 'MIL'},
-                    {'label': 'Cleveland Cavaliers', 'value': 'CLE'},
-                    {'label': 'San Antonio Spurs', 'value': 'SAS'},
-                    {'label': 'Oklahoma City Thunder', 'value': 'OKC'},
-                    {'label': 'Miami Heat', 'value': 'MIA'},
-                    {'label': 'Atlanta Hawks', 'value': 'ATL'},
-                    {'label': 'Sacramento Kings', 'value': 'SAC'},
-                    {'label': 'Portland Trail Blazers', 'value': 'POR'},
-                    {'label': 'Orlando Magic', 'value': 'ORL'},
-                    {'label': 'Minnesota Timberwolves', 'value': 'MIN'},
-                    {'label': 'Indiana Pacers', 'value': 'IND'},
-                    {'label': 'Denver Nuggets', 'value': 'DEN'},
-                    {'label': 'Detroit Pistons', 'value': 'DET'}
-                ],
-                id="teams",
-                value='LAL',
-            ),
-
-            html.Div([
-                html.H3('Estadísticas'),
-                dcc.RadioItems(
-                    options=[
-                        {'label': 'Triples intentados', 'value': '3PA'},
-                        {'label': 'Triples metidos', 'value': '3PM'},
-                        {'label': 'Robos', 'value': 'STL'},
-                        {'label': 'Minutos jugados', 'value': 'MIN'},
-                        {'label': 'Partidos jugados', 'value': 'GP'},
-                        {'label': 'Puntos logrados', 'value': 'PTS'}
-                    ],
-                    id="columnas",
-                    value='3PM',
-                ),
-            ],
-            ),
-        ]),
-        dcc.Graph(id='graph'),
-        html.H3("Year:"),
-        dcc.Slider(
-            id='year',
-            min=df_EL['Season'].min(),
-            max=df_EL['Season'].max(),
-            value=df_EL['Season'].min(),
-            marks={str(year): str(year) for year in df_EL['Season']},
-            step=None
+        dbc.Row(
+            [
+                dbc.Col(dbc.Card(dbc.CardBody(html.Div([html.H3('Tipo de grafico'),
+                                                        dcc.Dropdown(
+                                                            id='graphType',
+                                                            options=[
+                                                                {'label': 'Tarta', 'value': 'pie'},
+                                                                {'label': 'Barras', 'value': 'bar'}
+                                                            ],
+                                                            value='pie',
+                                                            style={'font-size':'16px'}
+                                                        ),
+                                                        html.Br(),
+                                                        html.Div([
+                                                            html.H3('Equipo'),
+                                                            dcc.Dropdown(
+                                                                options=[
+                                                                    {'label': 'Los Angeles Lakers', 'value': 'LAL'},
+                                                                    {'label': 'Toronto Raptors', 'value': 'TOR'},
+                                                                    {'label': 'Utah Jazz', 'value': 'UTA'},
+                                                                    {'label': 'Philadelphia 76ers', 'value': 'PHI'},
+                                                                    {'label': 'Boston Celtics', 'value': 'BOS'},
+                                                                    {'label': 'Golden State Warriors', 'value': 'GSW'},
+                                                                    {'label': 'Los Angeles Clippers', 'value': 'LAC'},
+                                                                    {'label': 'New Orleans Pelicans', 'value': 'NOP'},
+                                                                    {'label': 'Charlotte Bobcats', 'value': 'CHA'},
+                                                                    {'label': 'Phoenix Suns', 'value': 'PHX'},
+                                                                    {'label': 'Washington Wizards', 'value': 'WAS'},
+                                                                    {'label': 'Memphis Grizzlies', 'value': 'MEM'},
+                                                                    {'label': 'New York Knicks', 'value': 'NYK'},
+                                                                    {'label': 'Dallas Mavericks', 'value': 'DAL'},
+                                                                    {'label': 'Milwakee Bucks', 'value': 'MIL'},
+                                                                    {'label': 'Cleveland Cavaliers', 'value': 'CLE'},
+                                                                    {'label': 'San Antonio Spurs', 'value': 'SAS'},
+                                                                    {'label': 'Oklahoma City Thunder', 'value': 'OKC'},
+                                                                    {'label': 'Miami Heat', 'value': 'MIA'},
+                                                                    {'label': 'Atlanta Hawks', 'value': 'ATL'},
+                                                                    {'label': 'Sacramento Kings', 'value': 'SAC'},
+                                                                    {'label': 'Portland Trail Blazers', 'value': 'POR'},
+                                                                    {'label': 'Orlando Magic', 'value': 'ORL'},
+                                                                    {'label': 'Minnesota Timberwolves', 'value': 'MIN'},
+                                                                    {'label': 'Indiana Pacers', 'value': 'IND'},
+                                                                    {'label': 'Denver Nuggets', 'value': 'DEN'},
+                                                                    {'label': 'Detroit Pistons', 'value': 'DET'}
+                                                                ],
+                                                                id="teams",
+                                                                value='LAL',
+                                                                style={'font-size':'16px'}
+                                                            ),
+                                                            html.Br(),
+                                                            html.Div([
+                                                                html.H3('Tipo estadistica'),
+                                                                dcc.RadioItems(
+                                                                    options=[
+                                                                        {'label': '__Triples intentados', 'value': '3PA'},
+                                                                        {'label': '__Triples metidos', 'value': '3PM'},
+                                                                        {'label': '__Robos', 'value': 'STL'},
+                                                                        {'label': '__Minutos jugados', 'value': 'MIN'},
+                                                                        {'label': '__Partidos jugados', 'value': 'GP'},
+                                                                        {'label': '__Puntos logrados', 'value': 'PTS'}
+                                                                    ],
+                                                                    id="columnas",
+                                                                    value='3PM',
+                                                                    style={'font-size':'16px'},
+                                                                    labelStyle={'display': 'block'}
+                                                                )
+                                                            ]),
+                                                        ]),])), style={'height':'70vh'}), width=4),
+                dbc.Col(dbc.Card(dbc.CardBody(html.Div([dcc.Graph(id='graph'),
+                                                        html.H3("Temporada:"),
+                                                        dcc.Slider(
+                                                            id='year',
+                                                            min=df_EL['Season'].min(),
+                                                            max=df_EL['Season'].max(),
+                                                            value=df_EL['Season'].min(),
+                                                            marks={str(year): str(year) for year in df_EL['Season']},
+                                                            step=None
+                                                        )])), style={'height':'70vh'}), width=8)
+            ]
         )
     ]), ])
 
@@ -210,6 +244,7 @@ def update_figure(team, columna, year, tipo):
                 "annotations": [
                     {"font": {"size": 20},
                      "showarrow": False,
+                     "text":"",
                      "x": 0.20,
                      "y": 1
                      },
